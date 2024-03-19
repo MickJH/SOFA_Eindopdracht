95045ed05e1717fdfdd8e21697e821cedb7388ac
Juliet van Bezooijen <jay.vanbezooijen@student.avans.nl>
2024-03-19 13:11:17 +0100

    Basic implmentation observer pattern forum

---
+++ AvansDevOps/Observer/Interfaces/INotificationSubject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvansDevOps.Observer.Interfaces
+{
+    public interface INotificationSubject
+    {
+        void RegisterObserver(INotificationObserver observer);
+        void RemoveObserver(INotificationObserver observer);
+        void NotifyObservers(string message);
+    }
+}
--- AvansDevOps/State/Forums/Forum.cs
+++ AvansDevOps/State/Forums/Forum.cs
@@ -1,4 +1,5 @@
-﻿using AvansDevOps.State.Forums.States;
+﻿using AvansDevOps.Observer.Interfaces;
+using AvansDevOps.State.Forums.States;
 using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
@@ -8,11 +9,12 @@ using System.Threading.Tasks;
 
 namespace AvansDevOps.State.Forums
 {
-    public class Forum
+    public class Forum : INotificationSubject
     {
         public IForumState CurrentState { get; set; }
         public string Title { get; private set; }
         private List<Message> Messages;
+        private readonly List<INotificationObserver> _notificationObservers;
 
         public Forum(int backlogId ,string topic)
         {
@@ -28,5 +30,24 @@ namespace AvansDevOps.State.Forums
 
         public void Open() => CurrentState.Open(this);
         public void Close() => CurrentState.Close(this);
+
+        public void RegisterObserver(INotificationObserver observer)
+        {
+            _notificationObservers.Add(observer);
+        }
+
+        public void RemoveObserver(INotificationObserver observer)
+        {
+            int i = _notificationObservers.IndexOf(observer);
+            _notificationObservers.RemoveAt(i);
+        }
+
+        public void NotifyObservers(string message)
+        {
+            foreach (var observer in _notificationObservers)
+            {
+                observer.Notify(message);
+            }
+        }
     }
 }
